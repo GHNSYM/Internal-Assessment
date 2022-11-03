4671255fa49691b4faee35c17a3bb86a04aa664a
GHNSYM <103853143+GHNSYM@users.noreply.github.com>
2022-11-04 00:41:03 +0530

    Update roll-search.php

--- admin/roll-search.php
+++ admin/roll-search.php
@@ -2,12 +2,12 @@
 
 
 <?php
-    if(isset($_SESSION['search']))
+    if(isset($_POST['submit']))
     {
-        $roll=$_SESSION['search'];
-        echo $_SESSION['search'];
+        $search=mysqli_real_escape_string($conn,$_POST['search']);
+
         $sql="SELECT * FROM student WHERE
-        roll_no like '%$roll%'";
+        roll_no like '%$search%' OR name LIKE '%$search%' OR regno LIKE '%$search%'";
 
         $res=mysqli_query($conn,$sql);
 
@@ -17,69 +17,76 @@
 
             if($count>0)
             {
+                ?>
+                <div class="main">
+                        <div class="student">
+                            <table class="student-table">
+                                <br>
+                                <br>
+                                <h1>Search Results</h1>
+                                <br>
+                                <br>
+                                <tr>
+                                    <th>S.N.</th>
+                                    <th>Roll No.</th>
+                                    <th>Reg. No.</th>
+                                    <th>Name</th>
+                                    <th>DOB</th>
+                                    <th>Semester</th>
+                                    <th>Course</th>
+                                    <th>Batch</th>
+                                    <th>Contact</th>
+                                    <th>Action</th>
+                                </tr>
+
+                <?php
                 $sn=1;
                 while($row=mysqli_fetch_assoc($res))
                 {
-                    $name=$row['name'];
                     $roll=$row['roll_no'];
-                    $regno=$row['regno'];
-                    $dob=$row['dob'];
-                    $sem=$row['current_sem'];
-                    $batch=$row['batch'];
-                    $contact=$row['contact'];
+                    ?>
+                    
+                                <tr>
+                                    <td><?php echo $sn++;?></td>
+                                    <td><?php echo $row['roll_no'];?></td>
+                                    <td><?php echo $row['regno'];?></td>
+                                    <td><?php echo $row['name'];?></td>
+                                    <td><?php echo $row['dob'];?></td>
+                                    <td><?php echo $row['current_sem'];?></td>
+                                    <td><?php echo $row['course'];?></td>
+                                    <td><?php echo $row['batch'];?></td>
+                                    <td><?php echo $row['contact'];?></td>
+                                    <td>
+                                    <div class="dropdown">
+                                        <button class="dropbtn">Options ⇩</button>
+                                        <div class="dropdown-content">
+                                            <a href="<?php echo SITEURL."admin/marks.php?roll=$roll"?>">Marks</a>
+                                            <a href="<?php echo SITEURL."admin/update-student.php?roll=$roll"?>">Update</a>
+                                            <a class="dropdel" href="<?php echo SITEURL."admin/delete-student.php?roll=$roll"?>">Delete</a>
+                                        </div>
+                                    </div>
+                                    </td>
+                                </tr>
+                    <?php
                 }
+                ?>
+                            </table>  
+                        </div>  
+                    </div>
+                <?php
             }
-        }
-        else
-        {
-            $_SESSION['search_fail']="Couldn't Search Roll No.";
+            else
+            {
+                $_SESSION['search_fail']="There are no results matching your search.";
 
-            header('location:'.SITEURL.'admin/admin-home.php');
-        }    
+                header('location:'.SITEURL.'admin/admin-home.php');
+            } 
+        }
+           
     }
 ?>
                     
-<div class="main">
-    <div class="student">
-    <table class="student-table">
-        <br>
-        <br>
-        <h1>Search Results</h1>
-        <br>
-        <br>
-        <tr>
-            <th>S.N.</th>
-            <th>Roll No.</th>
-            <th>Reg. No.</th>
-            <th>Name</th>
-            <th>DOB</th>
-            <th>Semester</th>
-            <th>Batch</th>
-            <th>Contact</th>
-            <th>Action</th>
-        </tr>
-        <tr>
-            <td><?php echo $sn++;?></td>
-            <td><?php echo $roll;?></td>
-            <td><?php echo $regno;?></td>
-            <td><?php echo $name;?></td>
-            <td><?php echo $dob;?></td>
-            <td><?php echo $sem;?></td>
-            <td><?php echo $batch;?></td>
-            <td><?php echo $contact;?></td>
-            <td>
-            <div class="dropdown">
-                <button class="dropbtn">Options ⇩</button>
-                <div class="dropdown-content">
-                    <a href="<?php echo SITEURL."admin/marks.php?roll=$roll"?>">Marks</a>
-                    <a href="<?php echo SITEURL."admin/update-student.php?roll=$roll"?>">Update</a>
-                    <a href="<?php echo SITEURL."admin/delete-student.php?roll=$roll"?>">Delete</a>
-                </div>
-            </div>
-            </td>
-        </tr>
-    </table>  
-</div>  
-</div>
 
 <?php include('partials/footer.php');?>
+
+    
